c0a1d03fab647ac5f13a40afcbe917990eb0c34a
Sughit <andreisorici2007@gmail.com>
2024-09-30 23:05:57 +0300

    criptarea

--- criptare.php
+++ criptare.php
@@ -1,38 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <!--Bootstrap files-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <title>Criptare</title>
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="index.html">Acasă</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="teorie.html">Teorie</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Criptează</a>
-              </li>
-              <li class="nav-item"></li>
-                <a class="nav-link" href="register.html">Înregistrează-te</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-    </nav>
-</body>
-</html>
-<php?
+<?php
+
+$code = filter_input(INPUT_POST, 'code', FILTER_SANITIZE_STRING);
 
 ?>
+
+<?php if ($code) : ?>
+    <p>You selected <?php echo $code ?></span></p>
+    <p><a href="index.html">Back to the form</a></p>
+<?php else : ?>
+    <p>You did not select any code</p>
+<?php endif ?>
